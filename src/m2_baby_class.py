275c85ce9d4844b27215b27902f85f140533c0ca
cochraef <cochraef@rose-hulman.edu>
2018-09-20 16:32:14 -0400

    module 2 done

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -2,8 +2,8 @@
 A   Baby   class and methods that use the Baby class.
 
 Authors: David Mutchler, Vibha Alangar, Dave Fisher, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Evan Cochrane.
+"""  # 1: DONE PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -39,7 +39,7 @@ def main():
 
 
 # ----------------------------------------------------------------------
-# TODO: 2. Implement a class called   Baby   that has a constructor and
+# 2: DONE Implement a class called   Baby   that has a constructor and
 #   two methods, as described below.  Your finished Baby class should
 #   cause the code above to display the expected output.  Hint: Your
 #   class will need instance variables that you must figure out.
@@ -93,6 +93,27 @@ def main():
 #          """ Brief description of what objects of the class 'are'. """
 #
 ########################################################################
+
+class Baby(object):
+
+    def __init__(self, name):
+        self.name = name
+        self.times_called = 0
+
+        print('Hello baby {}!'.format(self.name))
+
+    def feed_baby(self):
+        self.times_called = 0
+        print('Thank you for feeding baby {}.'.format(self.name))
+
+    def hour_passes(self):
+        if self.times_called == 0:
+            print('Baby {} is sleeping.'.format(self.name))
+        elif self.times_called == 1:
+            print('Baby {} is awake. Time for food.'.format(self.name))
+        else:
+            print('Baby {} is CRYING uncontrollably! Feed the Baby!'.format(self.name))
+        self.times_called = self.times_called + 1
 
 
 # ----------------------------------------------------------------------
